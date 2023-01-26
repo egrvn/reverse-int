d0bac091625be373f483da6e4e193067e404ff9d
frmdth <egrv.nkt@gmail.com>
2023-01-26 22:02:08 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    const reversed = number.toString().split('').reverse().join('');
+    return parseInt(reversed);    
 }
